ff5dad62e05f95e83569d78ec9118152c30be128
jasonoh22 <jasonoh22@naver.com>
2023-12-07 01:30:28 +0900

    feat : article detail.html

--- src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,5 +20,10 @@ public class ArticleController {
         List<Article> articleList = articleService.getList();
         model.addAttribute("articleList", articleList);
         return "domain/article/article/list";
+    }
+
+    @GetMapping(value = "/article/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "domain/article/article/detail";
     }
 }
