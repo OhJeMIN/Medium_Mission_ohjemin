4297b21786f514ede32d7f3e1dd1d8312e057ede
jasonoh22 <jasonoh22@naver.com>
2023-12-06 22:56:57 +0900

    feat : list.html

--- src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
@@ -1,15 +1,23 @@
 package com.ll.medium.domain.article.article.controller;
 
+import com.ll.medium.domain.article.article.entity.Article;
+import com.ll.medium.domain.article.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ArticleController {
+    private final ArticleRepository articleRepository;
 
     @GetMapping("/article/list")
-    @ResponseBody
-    public String list(){
-        return "aritlce/list";
+    public String list(Model model){
+        List<Article> articleList = this.articleRepository.findAll();
+        model.addAttribute("articleList", articleList);
+        return "domain/article/article/list";
     }
 }
