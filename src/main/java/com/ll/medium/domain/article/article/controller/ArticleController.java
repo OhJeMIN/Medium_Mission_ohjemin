8863fe6e993a0978a15f036620a86a0186d161c4
jasonoh22 <jasonoh22@naver.com>
2023-12-07 16:31:52 +0900

    feat : enRequestMapping

--- src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
@@ -7,22 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/article")
 @RequiredArgsConstructor
 public class ArticleController {
     private final ArticleService articleService;
 
-    @GetMapping("/article/list")
+    @GetMapping("/list")
     public String list(Model model){
         List<Article> articleList = articleService.getList();
         model.addAttribute("articleList", articleList);
         return "domain/article/article/list";
     }
 
-    @GetMapping(value = "/article/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Article article = articleService.getArticle(id);
         model.addAttribute("article", article);
