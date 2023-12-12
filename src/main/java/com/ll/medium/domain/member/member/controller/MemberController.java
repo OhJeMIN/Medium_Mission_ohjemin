e64e43320093ee608f2153676ea29f30f66157c4
jasonoh22 <jasonoh22@naver.com>
2023-12-12 16:59:51 +0900

    feat : login.html && GetMapping login

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -48,5 +48,10 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "domain/member/member/login";
+    }
 }
 
