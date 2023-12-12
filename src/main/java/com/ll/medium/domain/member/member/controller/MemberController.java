b25cb2fef35b2fbaac1136e4cf8b5e8576928120
jasonoh22 <jasonoh22@naver.com>
2023-12-12 15:47:47 +0900

    feat : PostMapping join

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -2,9 +2,12 @@ package com.ll.medium.domain.member.member.controller;
 
 import com.ll.medium.domain.member.member.form.MemberForm;
 import com.ll.medium.domain.member.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,4 +20,22 @@ public class MemberController {
     public String join(MemberForm memberForm){
         return "domain/member/member/join";
     }
+
+    @PostMapping("/join")
+    public String signup(@Valid MemberForm memberForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "domain/member/member/join";
+        }
+
+        if (!memberForm.getPassword().equals(memberForm.getPasswordConfirm())) {
+            bindingResult.rejectValue("getPasswordConfirm", "passwordInCorrect",
+                    "2개의 패스워드가 일치하지 않습니다.");
+            return "domain/member/member/join";
+        }
+
+        memberService.join(memberForm.getUsername(), memberForm.getPassword());
+
+        return "redirect:/";
+    }
 }
+
