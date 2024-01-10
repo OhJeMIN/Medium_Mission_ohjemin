ae2f930a6e13db2b6a923bdd47e34f1ce777ede7
jasonoh22 <jasonoh22@naver.com>
2024-01-10 14:48:27 +0900

    feat : ëdocs¬위해 파일 수정

--- src/test/java/com/ll/medium/MediumApplicationTests.java
+++ src/test/java/com/ll/medium/MediumApplicationTests.java
@@ -1,16 +1,21 @@
 package com.ll.medium;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Transactional
 public class MediumApplicationTests {
 
-
+    @DisplayName("t1")
     @Test
-    void contextLoads() {
+    void t1() {
     }
     /*@Test
     void testJpa() {
